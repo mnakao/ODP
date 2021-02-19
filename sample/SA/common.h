c39ecfdd4e6c463dea3ad2ef3f866243f64dbbe0
Masahiro Nakao <masahiro.nakao@riken.jp>
2021-02-19 19:53:05 +0900

    Add grid_rc.c

--- sample/SA/common.h
+++ sample/SA/common.h
@@ -11,6 +11,8 @@
 #define MAX_FILENAME_LENGTH 256
 #define NOT_DEFINED -1
 #define ERROR(...) do{fprintf(stderr, __VA_ARGS__); exit(0);}while(0)
+#define WIDTH(v,h) ((v)/(h))
+#define HEIGHT(v,h) ((v)%(h))
 
 bool accept(const int nodes, const int current_diameter, const int diameter,
 	    const double current_ASPL, const double ASPL, const double temp, const bool enable_ASPL_priority);
