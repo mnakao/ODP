6cbbb15cbfa7b71f6788f1154377bcf9ff359416
Masahiro Nakao <masahiro.nakao@riken.jp>
2021-08-27 21:50:48 +0900

    When executing POPCNT() with the Fujitsu compiler (including clang), it sometimes bugs (only for 64K x 64 matrix?)

--- aspl.c
+++ aspl.c
@@ -51,7 +51,9 @@ static void aspl_mat(const int* restrict adjacency, int *diameter, long *sum, do
 	       _elements, _symmetries, _enable_avx2);
 
     uint64_t num = 0;
+#ifndef __FUJITSU
 #pragma omp parallel for reduction(+:num)
+#endif
     for(int i=0;i<_elements*_nodes;i++)
       num += POPCNT(_B[i]);
 
@@ -94,7 +96,9 @@ static void aspl_mat_saving(const int* restrict adjacency, int *diameter, long *
 		       _symmetries, _enable_avx2);
 
       uint64_t num = 0;
+#ifndef __FUJITSU
 #pragma omp parallel for reduction(+:num)
+#endif
       for(int i=0;i<CPU_CHUNK*_nodes;i++)
         num += POPCNT(_B[i]);
 
